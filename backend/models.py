439d0c0985a2ca7fdf3c2fcdd67bddc4de36c59f
sivasangar2006 <sivasangar2786@gmail.com>
2026-06-05 13:59:25 +0530

    Initial backend works done

---
+++ backend/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.sql import func
+from database import Base  
+
+
+class Prediction(Base):
+    
+    
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    
+ 
+    filename = Column(String, nullable=False)
+    
+    
+    mood = Column(String, nullable=False)
+    
+
+    confidence = Column(Float, nullable=False)
+    prob_happy = Column(Float, default=0.0)
+    prob_energetic = Column(Float, default=0.0)
+    prob_angry = Column(Float, default=0.0)
+    prob_sad = Column(Float, default=0.0)
+    prob_relaxed = Column(Float, default=0.0)
+    
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
